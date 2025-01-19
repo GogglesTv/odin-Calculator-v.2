1208fecb5b67b0846f8a3b467cde3af2a8578ff3
GogglesTv <jalenmarquise.moore@gmail.com>
2025-01-18 19:34:35 -0600

    Operate function returns operation function based on selected operator
    Put num1 and num2 parameters within each operator function

--- script.js
+++ script.js
@@ -2,24 +2,36 @@
 
 let num1;
 let num2;
-let num3;
+let operator = "";
 
-function add() {
+function add(num1, num2) {
   let sum = num1 + num2;
   return sum;
 }
 
-function subtract() {
+function subtract(num1, num2) {
   let difference = num1 - num2;
   return difference;
 }
 
-function multiply() {
+function multiply(num1, num2) {
   let product = num1 * num2;
   return product;
 }
 
-function divide() {
+function divide(num1, num2) {
   let quotient = num1 / num2;
   return quotient;
+}
+
+function operate() {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else {
+    return divide(num1, num2);
+  }
 }
