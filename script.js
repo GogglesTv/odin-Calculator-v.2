050e317eff56b998f61905aaa2b41df7adffce7a
GogglesTv <jalenmarquise.moore@gmail.com>
2025-01-18 19:15:00 -0600

    Created the basic operations for the calculator

---
+++ script.js
@@ -0,0 +1,25 @@
+"use script";
+
+let num1;
+let num2;
+let num3;
+
+function add() {
+  let sum = num1 + num2;
+  return sum;
+}
+
+function subtract() {
+  let difference = num1 - num2;
+  return difference;
+}
+
+function multiply() {
+  let product = num1 * num2;
+  return product;
+}
+
+function divide() {
+  let quotient = num1 / num2;
+  return quotient;
+}
