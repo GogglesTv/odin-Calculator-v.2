1566052e29c744623cedf99fc66a01adab58b148
GogglesTv <jalenmarquise.moore@gmail.com>
2025-01-28 22:55:53 -0600

    Calculator displays which operator is selected
    When the equals button is selected the result of the equation displays

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 let num1;
 let num2;
-let operator = "";
+let operation = "";
 let displayStr = "";
-let displayNum;
 
 const display = document.querySelector(".display");
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals");
 
 function add(num1, num2) {
   let sum = num1 + num2;
@@ -29,26 +30,54 @@ function divide(num1, num2) {
   return quotient;
 }
 
-function operate() {
-  if (operator === "+") {
+function operate(operation, num1, num2) {
+  if (operation === "+") {
     return add(num1, num2);
-  } else if (operator === "-") {
+  } else if (operation === "-") {
     return subtract(num1, num2);
-  } else if (operator === "*") {
+  } else if (operation === "*") {
     return multiply(num1, num2);
   } else {
     return divide(num1, num2);
   }
 }
 
+function resetDisplay() {
+  displayStr = "";
+  display.textContent = "0";
+}
+
+function resetOperatorColor() {
+  operators.forEach((operator) => {
+    operator.style.backgroundColor = "";
+  });
+}
+
 numbers.forEach((number) => {
   number.addEventListener("click", () => {
     const numberValue = number.getAttribute("data-value");
-    console.log(numberValue);
     displayStr += numberValue;
     display.textContent = displayStr;
-    displayNum = parseInt(displayStr);
+
+    if (num1 !== undefined) {
+      num2 = parseInt(displayStr);
+    }
   });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    operator.style.backgroundColor = "#6a6a6a";
+    operation = operator.textContent;
+    num1 = parseInt(displayStr);
+    resetDisplay();
+  });
+});
+
+equals.addEventListener("click", () => {
+  resetOperatorColor();
+  displayStr = operate(operation, num1, num2);
+  display.textContent = displayStr;
 });
 
 console.log(numbers);
