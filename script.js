fbe07c97db02dd9106b04a49ed22a2e2554446c7
GogglesTv <jalenmarquise.moore@gmail.com>
2025-02-12 22:13:55 -0600

    Operator can be chosen using the keyboard.
    Also refactored and created a function for choosing an operator.
    Added classes to the operator buttons for keyboard events.

--- script.js
+++ script.js
@@ -63,6 +63,16 @@ function resetOperator() {
   });
 }
 
+function operatorChosen() {
+  if (displayStr === "") {
+    displayStr = "0";
+  }
+
+  num1 = parseFloat(displayStr);
+
+  resetDisplay();
+}
+
 document.addEventListener("keyup", (e) => {
   if (parseFloat(e.key) <= 9 || parseFloat(e.key) >= 0) {
     const numberValue = parseFloat(e.key);
@@ -91,6 +101,51 @@ document.addEventListener("keyup", (e) => {
     }
   }
 
+  const plus = document.querySelector(".plus");
+  const minus = document.querySelector(".minus");
+  const multiply = document.querySelector(".multiply");
+  const divide = document.querySelector(".divide");
+
+  if (e.key === "+") {
+    operation = e.key;
+    plus.style.backgroundColor = "#6a6a6a";
+    operatorChosen();
+  } else if (e.key === "-") {
+    operation = e.key;
+    minus.style.backgroundColor = "#6a6a6a";
+    operatorChosen();
+  } else if (e.key === "*") {
+    operation = e.key;
+    multiply.style.backgroundColor = "#6a6a6a";
+    operatorChosen();
+  } else if (e.key === "/") {
+    operation = e.key;
+    divide.style.backgroundColor = "#6a6a6a";
+    operatorChosen();
+  } else if (e.key === "=" || e.key === "Enter") {
+    if (operation !== "") {
+      if (percentProb === true) {
+        if (percentNum === num2) {
+          num2 = (num1 / 100) * num2;
+          displayStr = operate(operation, num1, num2);
+          result = operate(operation, num1, num2);
+          console.log(num1, operation, num2, "=", result);
+          percentProb = false;
+          if (displayStr % 1 !== 0) {
+            displayStr = 1 * parseFloat(displayStr).toFixed(3);
+            display.textContent = displayStr;
+          }
+        }
+      } else {
+        num2 = parseFloat(displayStr);
+        displayStr = operate(operation, num1, num2);
+        result = operate(operation, num1, num2);
+        console.log(num1, operation, num2, "=", result);
+      }
+    }
+    display.textContent = displayStr;
+    resetOperator();
+  }
   console.log(e.key, e);
 });
 
@@ -127,13 +182,7 @@ operators.forEach((operator) => {
   operator.addEventListener("click", () => {
     operator.style.backgroundColor = "#6a6a6a";
     operation = operator.textContent;
-    if (displayStr === "") {
-      displayStr = "0";
-    }
-
-    num1 = parseFloat(displayStr);
-
-    resetDisplay();
+    operatorChosen();
   });
 });
 
